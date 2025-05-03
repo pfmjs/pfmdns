8444903df7fd62537bdb259516102383032a7c76
PFM <pradhananjana33@gmail.com>
2025-05-03 14:21:47 +0530

    .

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // Serve index.html as fallback
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   const filePath = path.join(__dirname, "sites", req.subdomain, "index.html");
   if (fs.existsSync(filePath)) {
     res.sendFile(filePath);
